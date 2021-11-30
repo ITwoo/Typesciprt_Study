3da91996b01a4a1001c90ebf23eb5a8b6789259a
ITWoo <chldngur101@gmail.com>
2021-11-30 16:34:17 +0900

    finish

---
+++ ch12/src/fake/index.ts
@@ -0,0 +1,3 @@
+import {IFake, makeFakeData} from './makeFakeData'
+import { writeCsvFormatFakeData } from './writeCsvFakeData'
+export {IFake, makeFakeData, writeCsvFormatFakeData}
---
+++ ch12/src/mongodb/connect.ts
@@ -0,0 +1,3 @@
+import { MongoClient } from "mongodb";
+
+export const connect = (mongoUrl: string = 'mongodb://localhost:27017') => MongoClient.connect(mongoUrl)
